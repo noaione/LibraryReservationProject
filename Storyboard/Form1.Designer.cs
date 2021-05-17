845226b72cc5e3a341d89103236bd017cb00b0a1
noaione <noaione0809@gmail.com>
2021-05-17 13:02:23 +0700

    Add project files.

---
+++ Storyboard/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Storyboard
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new MaterialSkin.Controls.MaterialLabel();
+            this.txtInUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtInPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblRegister = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Depth = 0;
+            this.lblName.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblName.Location = new System.Drawing.Point(25, 28);
+            this.lblName.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(110, 18);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Program Name";
+            // 
+            // txtInUsername
+            // 
+            this.txtInUsername.Location = new System.Drawing.Point(28, 74);
+            this.txtInUsername.Name = "txtInUsername";
+            this.txtInUsername.Size = new System.Drawing.Size(155, 20);
+            this.txtInUsername.TabIndex = 2;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(25, 58);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(25, 103);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtInPassword
+            // 
+            this.txtInPassword.Location = new System.Drawing.Point(28, 119);
+            this.txtInPassword.Name = "txtInPassword";
+            this.txtInPassword.PasswordChar = '*';
+            this.txtInPassword.Size = new System.Drawing.Size(155, 20);
+            this.txtInPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(28, 146);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.AutoSize = true;
+            this.lblRegister.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblRegister.Location = new System.Drawing.Point(28, 189);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(112, 13);
+            this.lblRegister.TabIndex = 7;
+            this.lblRegister.Text = "Register new Account";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 338);
+            this.Controls.Add(this.lblRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtInPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtInUsername);
+            this.Controls.Add(this.lblName);
+            this.Name = "LoginForm";
+            this.Text = "Library Reservation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel lblName;
+        private System.Windows.Forms.TextBox txtInUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtInPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblRegister;
+    }
+}
+
