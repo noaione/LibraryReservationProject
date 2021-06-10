832c8f836d5a4fb977e5b929d2bf908a8b6081fb
KEVIN PRASETIA KUMALA <TP058198@mail.apu.edu.my>
2021-06-10 16:48:46 +0800

    aa

---
+++ LibraryReservation/UserRequest.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace LibraryReservation
+{
+    partial class frmSeeRequest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstRequest = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Request From User";
+            // 
+            // lstRequest
+            // 
+            this.lstRequest.FormattingEnabled = true;
+            this.lstRequest.ItemHeight = 20;
+            this.lstRequest.Location = new System.Drawing.Point(93, 103);
+            this.lstRequest.Name = "lstRequest";
+            this.lstRequest.Size = new System.Drawing.Size(305, 264);
+            this.lstRequest.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(506, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Approve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(506, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(93, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(620, 59);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Back To Home";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmSeeRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstRequest);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSeeRequest";
+            this.Text = "UserRequest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstRequest;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ LibraryReservation/UserRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryReservation
+{
+    public partial class frmSeeRequest : Form
+    {
+        public frmSeeRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Keep To Database, and tell User that his/her Request has been approve
+            //Kalau Bisa Bkin yg MessageBox.Show yg pilih" itu
+            //Yg Pilihannya
+            //(Yes, Cancle)
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Back to Home(Librarian)
+            //gw gk bisa bkin krn gk ada Home(Librarian) dsni gatau tiba" ilang
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Delete from Database, and tell User that his/her Request has been Rejected
+        }
+    }
+}
