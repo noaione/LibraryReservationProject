ed9a5e24a80bc18142c164b8753c311490b44667
KEVIN PRASETIA KUMALA <TP058198@mail.apu.edu.my>
2021-06-17 16:39:50 +0800

    Rubah Posisi Register | Tapi yg bagian txtInFullName & txtInUsername blm tuker

--- LibraryReservation/Register.Designer.cs
+++ LibraryReservation/Register.Designer.cs
@@ -32,17 +32,17 @@ namespace LibraryReservation
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegister));
             this.lblProject = new System.Windows.Forms.Label();
             this.lblLogin = new System.Windows.Forms.Label();
-            this.btnRegister = new System.Windows.Forms.Button();
             this.lblPassword = new System.Windows.Forms.Label();
-            this.txtInPassword = new System.Windows.Forms.TextBox();
+            this.txtInFullName = new System.Windows.Forms.TextBox();
             this.lblUsername = new System.Windows.Forms.Label();
             this.txtInUsername = new System.Windows.Forms.TextBox();
             this.lblLoginClick = new System.Windows.Forms.Label();
             this.lblRealName = new System.Windows.Forms.Label();
-            this.txtInFullName = new System.Windows.Forms.TextBox();
+            this.txtInPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtInPasswordConf = new System.Windows.Forms.TextBox();
             this.BtnShowHide = new System.Windows.Forms.PictureBox();
+            this.btnRegister = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.BtnShowHide)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,9 +50,10 @@ namespace LibraryReservation
             // 
             this.lblProject.AutoSize = true;
             this.lblProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProject.Location = new System.Drawing.Point(32, 16);
+            this.lblProject.Location = new System.Drawing.Point(48, 25);
+            this.lblProject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblProject.Name = "lblProject";
-            this.lblProject.Size = new System.Drawing.Size(100, 25);
+            this.lblProject.Size = new System.Drawing.Size(142, 37);
             this.lblProject.TabIndex = 15;
             this.lblProject.Text = "Register";
             // 
@@ -60,63 +61,60 @@ namespace LibraryReservation
             // 
             this.lblLogin.AutoSize = true;
             this.lblLogin.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblLogin.Location = new System.Drawing.Point(29, 270);
+            this.lblLogin.Location = new System.Drawing.Point(44, 415);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(133, 13);
+            this.lblLogin.Size = new System.Drawing.Size(194, 20);
             this.lblLogin.TabIndex = 14;
             this.lblLogin.Text = "Already have an Account?";
-            // 
-            // btnRegister
-            // 
-            this.btnRegister.Location = new System.Drawing.Point(32, 235);
-            this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(75, 23);
-            this.btnRegister.TabIndex = 13;
-            this.btnRegister.Text = "Register";
-            this.btnRegister.UseVisualStyleBackColor = true;
-            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(32, 144);
+            this.lblPassword.Location = new System.Drawing.Point(48, 222);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
             this.lblPassword.TabIndex = 12;
             this.lblPassword.Text = "Password";
             // 
-            // txtInPassword
+            // txtInFullName
             // 
-            this.txtInPassword.Location = new System.Drawing.Point(32, 160);
-            this.txtInPassword.Name = "txtInPassword";
-            this.txtInPassword.PasswordChar = '*';
-            this.txtInPassword.Size = new System.Drawing.Size(155, 20);
-            this.txtInPassword.TabIndex = 11;
+            this.txtInFullName.Location = new System.Drawing.Point(48, 111);
+            this.txtInFullName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInFullName.Name = "txtInFullName";
+            this.txtInFullName.Size = new System.Drawing.Size(230, 26);
+            this.txtInFullName.TabIndex = 11;
+            this.txtInFullName.TextChanged += new System.EventHandler(this.txtInUsername_TextChanged);
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(32, 104);
+            this.lblUsername.Location = new System.Drawing.Point(48, 160);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.Size = new System.Drawing.Size(83, 20);
             this.lblUsername.TabIndex = 10;
             this.lblUsername.Text = "Username";
             // 
             // txtInUsername
             // 
-            this.txtInUsername.Location = new System.Drawing.Point(32, 120);
+            this.txtInUsername.Location = new System.Drawing.Point(48, 185);
+            this.txtInUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtInUsername.Name = "txtInUsername";
-            this.txtInUsername.Size = new System.Drawing.Size(155, 20);
+            this.txtInUsername.Size = new System.Drawing.Size(230, 26);
             this.txtInUsername.TabIndex = 9;
+            this.txtInUsername.TextChanged += new System.EventHandler(this.txtInFullName_TextChanged_1);
             this.txtInUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInUsername_KeyPress);
             // 
             // lblLoginClick
             // 
             this.lblLoginClick.AutoSize = true;
             this.lblLoginClick.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.lblLoginClick.Location = new System.Drawing.Point(165, 270);
+            this.lblLoginClick.Location = new System.Drawing.Point(248, 415);
+            this.lblLoginClick.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLoginClick.Name = "lblLoginClick";
-            this.lblLoginClick.Size = new System.Drawing.Size(42, 13);
+            this.lblLoginClick.Size = new System.Drawing.Size(61, 20);
             this.lblLoginClick.TabIndex = 16;
             this.lblLoginClick.Text = "Sign in!";
             this.lblLoginClick.Click += new System.EventHandler(this.lblLoginClick_Click);
@@ -124,66 +122,86 @@ namespace LibraryReservation
             // lblRealName
             // 
             this.lblRealName.AutoSize = true;
-            this.lblRealName.Location = new System.Drawing.Point(32, 56);
+            this.lblRealName.Location = new System.Drawing.Point(48, 86);
+            this.lblRealName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRealName.Name = "lblRealName";
-            this.lblRealName.Size = new System.Drawing.Size(54, 13);
+            this.lblRealName.Size = new System.Drawing.Size(80, 20);
             this.lblRealName.TabIndex = 18;
             this.lblRealName.Text = "Full Name";
             // 
-            // txtInFullName
+            // txtInPassword
             // 
-            this.txtInFullName.Location = new System.Drawing.Point(32, 72);
-            this.txtInFullName.Name = "txtInFullName";
-            this.txtInFullName.Size = new System.Drawing.Size(155, 20);
-            this.txtInFullName.TabIndex = 17;
+            this.txtInPassword.Location = new System.Drawing.Point(48, 247);
+            this.txtInPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInPassword.Name = "txtInPassword";
+            this.txtInPassword.PasswordChar = '*';
+            this.txtInPassword.Size = new System.Drawing.Size(230, 26);
+            this.txtInPassword.TabIndex = 17;
+            this.txtInPassword.TextChanged += new System.EventHandler(this.txtInFullName_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 186);
+            this.label1.Location = new System.Drawing.Point(51, 286);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(137, 20);
             this.label1.TabIndex = 20;
             this.label1.Text = "Confirm Password";
             // 
             // txtInPasswordConf
             // 
-            this.txtInPasswordConf.Location = new System.Drawing.Point(32, 202);
+            this.txtInPasswordConf.Location = new System.Drawing.Point(48, 311);
+            this.txtInPasswordConf.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtInPasswordConf.Name = "txtInPasswordConf";
             this.txtInPasswordConf.PasswordChar = '*';
-            this.txtInPasswordConf.Size = new System.Drawing.Size(155, 20);
+            this.txtInPasswordConf.Size = new System.Drawing.Size(230, 26);
             this.txtInPasswordConf.TabIndex = 21;
             // 
             // BtnShowHide
             // 
             this.BtnShowHide.Image = ((System.Drawing.Image)(resources.GetObject("BtnShowHide.Image")));
-            this.BtnShowHide.Location = new System.Drawing.Point(200, 160);
+            this.BtnShowHide.Location = new System.Drawing.Point(300, 246);
+            this.BtnShowHide.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnShowHide.Name = "BtnShowHide";
-            this.BtnShowHide.Size = new System.Drawing.Size(32, 32);
+            this.BtnShowHide.Size = new System.Drawing.Size(48, 49);
             this.BtnShowHide.TabIndex = 22;
             this.BtnShowHide.TabStop = false;
             this.BtnShowHide.Click += new System.EventHandler(this.BtnShowHide_Click);
             // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(48, 363);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(112, 35);
+            this.btnRegister.TabIndex = 23;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.button1_Click);
+            // 
             // frmRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(320, 325);
+            this.ClientSize = new System.Drawing.Size(480, 500);
+            this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.BtnShowHide);
             this.Controls.Add(this.txtInPasswordConf);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblRealName);
-            this.Controls.Add(this.txtInFullName);
+            this.Controls.Add(this.txtInPassword);
             this.Controls.Add(this.lblLoginClick);
             this.Controls.Add(this.lblProject);
             this.Controls.Add(this.lblLogin);
-            this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.txtInPassword);
+            this.Controls.Add(this.txtInFullName);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.txtInUsername);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmRegister";
             this.Text = "Library Reservation";
+            this.Load += new System.EventHandler(this.frmRegister_Load);
             ((System.ComponentModel.ISupportInitialize)(this.BtnShowHide)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -194,16 +212,16 @@ namespace LibraryReservation
 
         private System.Windows.Forms.Label lblProject;
         private System.Windows.Forms.Label lblLogin;
-        private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.TextBox txtInPassword;
+        private System.Windows.Forms.TextBox txtInFullName;
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.TextBox txtInUsername;
         private System.Windows.Forms.Label lblLoginClick;
         private System.Windows.Forms.Label lblRealName;
-        private System.Windows.Forms.TextBox txtInFullName;
+        private System.Windows.Forms.TextBox txtInPassword;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtInPasswordConf;
         private System.Windows.Forms.PictureBox BtnShowHide;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
