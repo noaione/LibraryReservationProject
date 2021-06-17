4b9e615181ac183a2f9b6b95d7eb25679917d119
noaione <noaione0809@gmail.com>
2021-06-17 18:29:54 +0700

    Almost finalize the UserRequest Left the approval method

--- LibraryReservation/UserRequest.Designer.cs
+++ LibraryReservation/UserRequest.Designer.cs
@@ -31,75 +31,158 @@ namespace LibraryReservation
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lstRequest = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnReject = new System.Windows.Forms.Button();
+            this.btnGoBack = new System.Windows.Forms.Button();
+            this.txtReasonBox = new System.Windows.Forms.TextBox();
+            this.lblReason = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblNewInfoText = new System.Windows.Forms.Label();
+            this.lblOldInfoText = new System.Windows.Forms.Label();
+            this.lblOldInfo = new System.Windows.Forms.Label();
+            this.lblRequester = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 53);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(312, 37);
+            this.label1.Size = new System.Drawing.Size(141, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Request From User";
+            this.label1.Text = "Request Page";
             // 
             // lstRequest
             // 
             this.lstRequest.FormattingEnabled = true;
-            this.lstRequest.ItemHeight = 20;
-            this.lstRequest.Location = new System.Drawing.Point(93, 103);
+            this.lstRequest.Location = new System.Drawing.Point(32, 64);
+            this.lstRequest.Margin = new System.Windows.Forms.Padding(2);
             this.lstRequest.Name = "lstRequest";
-            this.lstRequest.Size = new System.Drawing.Size(305, 264);
+            this.lstRequest.Size = new System.Drawing.Size(248, 212);
             this.lstRequest.TabIndex = 1;
+            this.lstRequest.SelectedIndexChanged += new System.EventHandler(this.lstRequest_SelectedIndexChanged);
             // 
-            // button1
+            // btnAccept
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(506, 122);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 60);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Approve";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccept.Location = new System.Drawing.Point(32, 288);
+            this.btnAccept.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(111, 39);
+            this.btnAccept.TabIndex = 2;
+            this.btnAccept.Text = "Approve";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
             // 
-            // button2
+            // btnReject
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(506, 246);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(166, 60);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Reject";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnReject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReject.Location = new System.Drawing.Point(160, 288);
+            this.btnReject.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReject.Name = "btnReject";
+            this.btnReject.Size = new System.Drawing.Size(111, 39);
+            this.btnReject.TabIndex = 3;
+            this.btnReject.Text = "Reject";
+            this.btnReject.UseVisualStyleBackColor = true;
+            this.btnReject.Click += new System.EventHandler(this.btnReject_Click);
             // 
-            // button3
+            // btnGoBack
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(93, 379);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(620, 59);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Back To Home";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btnGoBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoBack.Location = new System.Drawing.Point(584, 296);
+            this.btnGoBack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGoBack.Name = "btnGoBack";
+            this.btnGoBack.Size = new System.Drawing.Size(88, 32);
+            this.btnGoBack.TabIndex = 4;
+            this.btnGoBack.Text = "Go Back";
+            this.btnGoBack.UseVisualStyleBackColor = true;
+            this.btnGoBack.Click += new System.EventHandler(this.btnGoBack_Click);
+            // 
+            // txtReasonBox
+            // 
+            this.txtReasonBox.Location = new System.Drawing.Point(288, 192);
+            this.txtReasonBox.Multiline = true;
+            this.txtReasonBox.Name = "txtReasonBox";
+            this.txtReasonBox.ReadOnly = true;
+            this.txtReasonBox.Size = new System.Drawing.Size(384, 80);
+            this.txtReasonBox.TabIndex = 5;
+            // 
+            // lblReason
+            // 
+            this.lblReason.AutoSize = true;
+            this.lblReason.Location = new System.Drawing.Point(288, 176);
+            this.lblReason.Name = "lblReason";
+            this.lblReason.Size = new System.Drawing.Size(44, 13);
+            this.lblReason.TabIndex = 6;
+            this.lblReason.Text = "Reason";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(464, 86);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(63, 13);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = "New Data";
+            // 
+            // lblNewInfoText
+            // 
+            this.lblNewInfoText.AutoSize = true;
+            this.lblNewInfoText.Location = new System.Drawing.Point(464, 104);
+            this.lblNewInfoText.Name = "lblNewInfoText";
+            this.lblNewInfoText.Size = new System.Drawing.Size(0, 13);
+            this.lblNewInfoText.TabIndex = 8;
+            // 
+            // lblOldInfoText
+            // 
+            this.lblOldInfoText.AutoSize = true;
+            this.lblOldInfoText.Location = new System.Drawing.Point(290, 104);
+            this.lblOldInfoText.Name = "lblOldInfoText";
+            this.lblOldInfoText.Size = new System.Drawing.Size(0, 13);
+            this.lblOldInfoText.TabIndex = 10;
+            // 
+            // lblOldInfo
+            // 
+            this.lblOldInfo.AutoSize = true;
+            this.lblOldInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOldInfo.Location = new System.Drawing.Point(290, 86);
+            this.lblOldInfo.Name = "lblOldInfo";
+            this.lblOldInfo.Size = new System.Drawing.Size(57, 13);
+            this.lblOldInfo.TabIndex = 9;
+            this.lblOldInfo.Text = "Old Data";
+            // 
+            // lblRequester
+            // 
+            this.lblRequester.AutoSize = true;
+            this.lblRequester.Location = new System.Drawing.Point(288, 64);
+            this.lblRequester.Name = "lblRequester";
+            this.lblRequester.Size = new System.Drawing.Size(59, 13);
+            this.lblRequester.TabIndex = 11;
+            this.lblRequester.Text = "Requester:";
             // 
             // frmSeeRequest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(706, 361);
+            this.Controls.Add(this.lblRequester);
+            this.Controls.Add(this.lblOldInfoText);
+            this.Controls.Add(this.lblOldInfo);
+            this.Controls.Add(this.lblNewInfoText);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblReason);
+            this.Controls.Add(this.txtReasonBox);
+            this.Controls.Add(this.btnGoBack);
+            this.Controls.Add(this.btnReject);
+            this.Controls.Add(this.btnAccept);
             this.Controls.Add(this.lstRequest);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmSeeRequest";
-            this.Text = "UserRequest";
+            this.Text = "Library Reservation - User Request";
             this.Load += new System.EventHandler(this.frmSeeRequest_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -110,8 +193,15 @@ namespace LibraryReservation
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lstRequest;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnReject;
+        private System.Windows.Forms.Button btnGoBack;
+        private System.Windows.Forms.TextBox txtReasonBox;
+        private System.Windows.Forms.Label lblReason;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblNewInfoText;
+        private System.Windows.Forms.Label lblOldInfoText;
+        private System.Windows.Forms.Label lblOldInfo;
+        private System.Windows.Forms.Label lblRequester;
     }
 }
--- LibraryReservation/UserRequest.cs
+++ LibraryReservation/UserRequest.cs
@@ -13,38 +13,113 @@ namespace LibraryReservation
     public partial class frmSeeRequest : Form
     {
         private static Users user;
+        private List<ReservationChange> reservationChanges = new List<ReservationChange>();
+
         public frmSeeRequest(Users u)
         {
             InitializeComponent();
             user = u;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private string FormatReservation(Reservation rr)
         {
-            //Keep To Database, and tell User that his/her Request has been approve
-            //Kalau Bisa Bkin yg MessageBox.Show yg pilih" itu
-            //Yg Pilihannya
-            //(Yes, Cancle)
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Program.ReplaceForm(new frmLibrarianHome(user), this);
-        }
-
-        private object frmLibrarianHome()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //Delete from Database, and tell User that his/her Request has been Rejected
+            return $"Room: {rr.Room.Name}\nTime: {rr.DateTimeText}\nDuration: {rr.Duration} minutes";
         }
 
         private void frmSeeRequest_Load(object sender, EventArgs e)
         {
+            DatabaseBridge db = new DatabaseBridge();
+            DataTable table = db.QueryDBAsTable("SELECT RC.*, R.UserID FROM ReservationChanges AS RC INNER JOIN Reservations AS R ON R.ReserveID = RC.ReserveID WHERE RC.ApprovedFlag = 0", true);
 
+            Dictionary<string, Rooms> savedRooms = new Dictionary<string, Rooms>();
+            foreach (DataRow row in table.Rows)
+            {
+                string newRoomID = row["NewRoom"].ToString();
+                Rooms newRoom;
+                if (savedRooms.ContainsKey(newRoomID))
+                {
+                    newRoom = savedRooms[newRoomID];
+                }
+                else
+                {
+                    newRoom = db.FindRoomByID(newRoomID, true);
+                }
+
+                Reservation oldReserve = db.FindReservationByID(row["ReserveID"].ToString());
+                int duration = int.Parse(row["DurationAfter"].ToString());
+                DateTime startRange = DateTime.Parse(row["DateAfter"].ToString()).ToUniversalTime();
+                DateTime timestamp = DateTime.Parse(row["Timestamp"].ToString()).ToUniversalTime();
+                string reason = row["Reason"].ToString();
+                int flag = int.Parse(row["ApprovedFlag"].ToString());
+
+                ReservationChange change = new ReservationChange(row["ChangesID"].ToString(), oldReserve, newRoom, startRange, duration, timestamp, reason, flag);
+                reservationChanges.Add(change);
+            }
+            db.Close();
+            reservationChanges.Sort((x, y) => x.Timestamp.CompareTo(y.Timestamp));
+            lstRequest.DataSource = reservationChanges;
+            lstRequest.DisplayMember = "DisplayData";
+            lstRequest.ValueMember = "ChangeID";
+            if (reservationChanges.Count < 1)
+            {
+                lblRequester.Text = "Requester: None";
+                lblOldInfoText.Text = "Please select a request first";
+                lblNewInfoText.Text = "Please select a request first";
+            }
+            else
+            {
+                lblRequester.Text = $"Requester: {reservationChanges[0].OldReservation.User.Name}";
+                lblOldInfoText.Text = FormatReservation(reservationChanges[0].OldReservation);
+                lblNewInfoText.Text = FormatReservation(reservationChanges[0].NewReservation);
+                txtReasonBox.Text = reservationChanges[0].Reason;
+                lstRequest.SelectedIndex = 0;
+            }
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            // Approve
+            // SQL Template:
+            // $"UPDATE Reservations SET DateTime = '{reservation.DateTimeSQL}', Duration = {reservation.Duration} WHERE ReserveID = '{reservation.ReserveID}'"
+            // $"UPDATE ReservationChanges SET ApprovedFlag = 1 WHERE ChangesID='{change.ChangeID}'"
+
+            // Update the main reservations data
+            // After that update the changes data to approved.
+        }
+
+        private void btnReject_Click(object sender, EventArgs e)
+        {
+            // Reject
+            ReservationChange change = (ReservationChange)lstRequest.SelectedItem;
+            int removePos = lstRequest.SelectedIndex;
+            DatabaseBridge db = new DatabaseBridge();
+            db.CommitToDB($"UPDATE ReservationChanges SET ApprovedFlag = -1 WHERE ChangesID='{change.ChangeID}'");
+            lstRequest.SelectedIndex = 0;
+            lstRequest.DataSource = null;
+            reservationChanges.RemoveAt(removePos);
+            lstRequest.DataSource = reservationChanges;
+            lstRequest.DisplayMember = "DisplayData";
+            lstRequest.ValueMember = "ChangeID";
+            MessageBox.Show($"Request {change.ChangeID} has been rejected!");
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            Program.ReplaceForm(new frmLibrarianHome(user), this);
+        }
+
+        private void lstRequest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ReservationChange change = (ReservationChange)lstRequest.SelectedItem;
+            if (change == null || reservationChanges == null)
+            {
+                // Ignore when everything is still froze.
+                return;
+            }
+            lblRequester.Text = $"Requester: {change.OldReservation.User.Name}";
+            lblOldInfoText.Text = FormatReservation(change.OldReservation);
+            lblNewInfoText.Text = FormatReservation(change.NewReservation);
+            txtReasonBox.Text = change.Reason;
         }
     }
 }
